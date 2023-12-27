65f0d3bc9a7f3ef3bb44d8c3a571c9b24d263681
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2023-12-26 23:01:44 -0300

    Update CarroController.js

--- apiCarro/src/controllers/CarroController.js
+++ apiCarro/src/controllers/CarroController.js
@@ -1,8 +1,9 @@
-const CarroService = require('../services/CarroService.js');
+const CarroService = require('../services/CarroService');
 
 module.exports = {
-    buscarTodos: async(req, res)=> {
-        let json = {error: '', result: []}
+    buscarTodos: async (req, res) => {
+        let json = {error:'', result:[]};
+
         let carros = await CarroService.buscarTodos();
 
         for(let i in carros){
@@ -11,6 +12,7 @@ module.exports = {
                 descricao: carros[i].modelo
             });
         }
+
         res.json(json);
     }
-}
+};
