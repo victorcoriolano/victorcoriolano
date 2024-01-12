10f55237f4ff080bfa5860141a18a5aadcdf343a
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2024-01-12 06:39:07 -0300

    Retirando repos do meu repo pessoal
    ;P

--- apiCarro/src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const CarroController = require('./controllers/CarroController');
-
-router.get('/carros', CarroController.buscarTodos);
-
-module.exports = router;
--- aprendendoNode/src/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const api = express();
-const routes = require('./router');
-
-api.use(express.json());
-api.use(routes);
-
-api.listen(4200);
